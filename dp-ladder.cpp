fd39f0dea35e290ad0424742223d8ba785e124e5
tarunagarwal99 <tarunprahladka965@gmail.com>
2021-09-07 22:30:56 +0530

    .

---
+++ dp-ladder.cpp
@@ -0,0 +1,44 @@
+
+//ladder problem 
+//we can only take steps of  1 or 2 to climb the ladder
+//we have to find out how many method we have to climb the ladder in this particular scenerio 
+
+// this code is showing TLE error in code because one computation is computed many times
+//to overcome with this problem we are usimg dynamic programming approach in which previously compuated value are stored in the array which we use further
+
+
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+int  dp[46];
+
+int help(int s,int n){
+    //here s is the number of step we have taken
+    if(s==n) return 1;   //base case  
+    if(s>n) return 0;
+    if(dp[s]!=-1) return dp[s];
+    int ans = help(s+1,n)+help(s+2,n);
+    dp[s]=ans;
+    return dp[s];
+}
+
+int main() {
+
+    int n;
+    cin>>n;
+    itn dp[46];
+    for (itn i = 0; i < 46; i++)
+    {
+        dp[i]=-1;
+    }
+    
+    cout<<help(0,n);
+
+
+
+    
+    return 0;
+}
